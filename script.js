d5fb1f302499ffb2eb37ec29db5da0f5699a7c21
Snezhko Vadim <sneshko00@bk.ru>
2020-01-16 21:08:33 +0300

    task-2

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 class MyArray {
   constructor(...elements) {
-    this.array = [...elements];
+    this.length=0;
+    this.array = {};
+    for(let i=0;i<elements.length; i++){
+      this.array[i]=elements[i];
+      this.length++;
+    }
   }
   printArray() {
     console.log(this.array);
   }
 
   getLength() {
-    let counter = 0;
-    while (this.array[counter]) {
-      counter++;
-    }
-    return counter;
+    return this.length;
   }
 
-  push(element) {
-    let length = this.array.length;
-    this.array[length] = element;
+  push(...element) {
+    let newLength=this.length+element.length;
+    for(let i=0;i<element.length;i++){
+      this.array[this.length] = element[i];
+      this.length++;
+    }
   }
   map(callback) {
-    let mapArray = [];
-    for (let i = 0; i < this.array.length; i++) {
+    let mapArray = {};
+    for (let i = 0; i < this.length; i++) {
       mapArray[i] = callback(this.array[i]);
     }
     return mapArray;
   }
 
   filter(callback) {
-    let filterArray = [];
-    for (let i = 0; i < this.array.length; i++) {
+    let filterArray = {};
+    for (let i = 0; i < this.length; i++) {
       if (callback(this.array[i])) {
         filterArray[i] = this.array[i];
       }
@@ -48,7 +52,7 @@ class MyArray {
       previousValue = i;
       j = 0;
     }
-    for (j; j < this.array.length; j++) {
+    for (j; j < this.length; j++) {
       result = callback(previousValue, this.array[j]);
       previousValue = result;
     }
@@ -57,18 +61,36 @@ class MyArray {
 
   includes(valueToFind, index = 0) {
     let i = index;
-    for (i; i < this.array.length; i++) {
+    for (i; i < this.length; i++) {
       if (valueToFind === this.array[i]) {
         return true;
       }
     }
-
     return false;
   }
 }
 
 let arr = new MyArray(1, 2, 3, 4, 5);
-let pushingValue = 2000;
+arr.printArray();
+arr.push(22,33,44);
+arr.printArray();
+let newMapArray = arr.map(function (element) {
+  return element * 10;
+});
+console.log(newMapArray);
+let newFilterArray = arr.filter(function (element) {
+  return element < 4;
+})
+console.log(newFilterArray);
+let reduceResult = arr.reduce(function (sum, element) {
+  return sum + element;
+}, 0)
+console.log(reduceResult);
+console.log("includes result(finding 3 from 3 element):");
+console.log(arr.includes(3, 3));
+console.log("includes result(finding 3):");
+console.log(arr.includes(3)); 
+/* let pushingValue = 2000;
 let newArray;
 
 console.log("Your array:");
@@ -107,4 +129,4 @@ console.log(`update method includes with starting index:`);
 console.log("includes result(finding 3 from 3 element):");
 console.log(arr.includes(3, 3));
 console.log("includes result(finding 3):");
-console.log(arr.includes(3));
+console.log(arr.includes(3)); */
